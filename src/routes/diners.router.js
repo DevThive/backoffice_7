ed58e6f1c2bf8ba294f92e3604382d2cc7559f60
dabbler2 <117023230+dabbler2@users.noreply.github.com>
2023-12-13 16:10:13 +0900

    영업시간 유효성 검사 추가

--- src/routes/diners.router.js
+++ src/routes/diners.router.js
@@ -1,19 +1,27 @@
-import express from 'express'
-import {DinersController} from '../controllers/diners.controller.js'
-import {authMiddleware} from '../middlewares/authMiddleware.js'
+import express from 'express';
+import { DinersController } from '../controllers/diners.controller.js';
+import { authMiddleware } from '../middlewares/authMiddleware.js';
 
-const router = express.Router()
-const dinersController = new DinersController()
+const router = express.Router();
+const dinersController = new DinersController();
 
 // 식당 등록
-router.post('/', dinersController.createDiner)
+router.post('/', dinersController.createDiner);
 // 전체 식당 조회
-router.get('/', dinersController.getDiners)
+router.get('/', dinersController.getDiners);
 // 특정 식당 조회
-router.get('/:dinerId', dinersController.findDiner, dinersController.getDiner)
+router.get('/:dinerId', dinersController.findDiner, dinersController.getDiner);
 // 식당 정보 수정
-router.patch('/:dinerId', dinersController.findDiner, dinersController.updateDiner)
+router.patch(
+  '/:dinerId',
+  dinersController.findDiner,
+  dinersController.updateDiner,
+);
 // 식당 삭제
-router.delete('/:dinerId', dinersController.findDiner, dinersController.deleteDiner)
+router.delete(
+  '/:dinerId',
+  dinersController.findDiner,
+  dinersController.deleteDiner,
+);
 
-export default router
+export default router;
