1a30a85f8a3bbc662bc5e5152aa685677545e458
데브티브 <thive8564@gmail.com>
2023-12-14 16:43:12 +0900

    add email

--- src/app.js
+++ src/app.js
@@ -1,22 +1,29 @@
-import path from 'path'
-import express from "express";
-import router from "./routes/index.js";
-import LogMiddleware from "./middlewares/log.middleware.js";
-import ErrorHandlingMiddlewqare from "./middlewares/error-handling.js";
+import express from 'express';
+import router from './routes/index.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const __dirname = path.resolve()
+import LogMiddleware from './middlewares/log.middleware.js';
+import ErrorHandlingMiddlewqare from './middlewares/error-handling.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const PORT = process.env.PORT;
 
-app.use('/frontend',express.static(__dirname+'/src/frontend'))
+app.use('/frontend', express.static(__dirname + '/src/frontend'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(LogMiddleware);
 app.use(ErrorHandlingMiddlewqare);
-app.use("/api", router);
+app.use('/api', router);
+
+app.use('/', express.static(path.join(__dirname, 'assets')));
+app.use(express.static('assets'));
 
 app.listen(PORT, () => {
-  console.log(PORT, "포트로 서버가 열렸습니다.");
+  console.log(PORT, '포트로 서버가 열렸습니다.');
 });
