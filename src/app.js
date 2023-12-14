cb5e4d1a296e9b48bcfad5284a90c1ad86047b8e
데브티브 <thive8564@gmail.com>
2023-12-14 16:43:12 +0900

    add email

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
-import express from "express";
-import router from "./routes/index.js";
-import LogMiddleware from "./middlewares/log.middleware.js";
-import ErrorHandlingMiddlewqare from "./middlewares/error-handling.js";
+import express from 'express';
+import router from './routes/index.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import LogMiddleware from './middlewares/log.middleware.js';
+import ErrorHandlingMiddlewqare from './middlewares/error-handling.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT;
@@ -10,8 +16,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(LogMiddleware);
 app.use(ErrorHandlingMiddlewqare);
-app.use("/api", router);
+app.use('/api', router);
+
+app.use('/', express.static(path.join(__dirname, 'assets')));
+app.use(express.static('assets'));
 
 app.listen(PORT, () => {
-  console.log(PORT, "포트로 서버가 열렸습니다.");
+  console.log(PORT, '포트로 서버가 열렸습니다.');
 });
---
+++ src/server/template.js
@@ -0,0 +1,13 @@
+export const temp = (data) => {
+  return `
+        <!DOCTYPE html>
+        <html style="margin: 0; padding: 0;">
+            <head>
+                <title>이메일 인증</title>
+            </head>
+            <body style="margin: 0; padding: 0; font-size:15px;">
+                <div>인증번호는 ${data} 입니다.</div>
+            </body>
+        </html>
+    `;
+};
