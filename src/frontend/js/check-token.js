697983db6ca02bc059b85b16b126d67b82ebd567
dabbler2 <117023230+dabbler2@users.noreply.github.com>
2023-12-15 21:52:11 +0900

    .

--- src/frontend/js/check-token.js
+++ src/frontend/js/check-token.js
@@ -12,7 +12,6 @@ async function checkAdmin() {
       const response = await axios.get(server + '/api/users/auth/admin/me', {
 		  headers});
       user = response.data.data;
-		console.log(user)
       if (user) {
         return user;
       }
