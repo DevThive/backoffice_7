29d0d6947371e9fc92275bd55ca14f7a232a5c91
데브티브 <thive8564@gmail.com>
2023-12-17 17:38:29 +0900

    front index, admin & connect diners

--- src/assets/api.js
+++ src/assets/api.js
@@ -8,14 +8,38 @@ function getSelf(callback) {
       authorization: `Bearer ${localStorage.getItem('token')}`,
     },
     success: function (response) {
-
       console.log(response.user);
 
       callback(response.user);
     },
     error: function (xhr, status, error) {
       if (status == 500) {
+        alert('로그인이 필요합니다.');
+      } else {
+        localStorage.clear();
+        alert('알 수 없는 문제가 발생했습니다. 관리자에게 문의하세요.');
+      }
+      window.location.href = '/';
+    },
+  });
+}
 
+function admingetSelf(callback) {
+  $.ajax({
+    type: 'GET',
+
+    url: '/api/users/auth/admin/me',
+
+    headers: {
+      authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+    success: function (response) {
+      console.log(response.user);
+
+      callback(response.user);
+    },
+    error: function (xhr, status, error) {
+      if (status == 500) {
         alert('로그인이 필요합니다.');
       } else {
         localStorage.clear();
---
+++ src/assets/js/check-token.js
@@ -0,0 +1,25 @@
+let user;
+let headers = {};
+
+// AccessToken 검사 및 헤더에 추가하는 함수
+async function checkAdmin() {
+  // 로컬 스토리지에서 AccessToken 가져오기
+  const authorization = localStorage.getItem('token');
+
+  try {
+    if (authorization) {
+      headers.authorization = `Bearer ${localStorage.getItem('token')}`
+      const response = await axios.get(server + '/api/users/auth/admin/me', {
+		  headers});
+      user = response.data.data;
+
+      if (user) {
+        return user;
+      }
+    }
+    return false;
+  } catch (e) {
+    console.log(e);
+  }
+  // AccessToken이 존재하는지 확인
+}
